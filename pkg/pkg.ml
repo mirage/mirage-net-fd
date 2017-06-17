291ad92ecf676b39f2848b7ae1c3888882e5f994
David Scott <dave@recoil.org>
2017-06-17 10:18:33 +0100

    Switch to jbuilder
    Signed-off-by: David Scott <dave@recoil.org>

--- pkg/pkg.ml
+++ pkg/pkg.ml
@@ -1,10 +1,3 @@
 #!/usr/bin/env ocaml
 #use "topfind"
-#require "topkg"
-open Topkg
-
-let () =
-  Pkg.describe "mirage-net-fd" @@ fun c ->
-  Ok [ Pkg.mllib "src/mirage-net-fd.mllib";
-       Pkg.test "test/test"
-  ]
+#require "topkg-jbuilder.auto"
